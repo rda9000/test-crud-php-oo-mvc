82fdc9343bdf8257235ce9868024ff1342c06946
rda9000 <rda9000@gmail.com>
2014-12-22 17:03:01 -0200

    mais um teste

--- controller/ContactsController.php
+++ controller/ContactsController.php
@@ -3,22 +3,23 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/projects/crud_mvc_oop/model/Autoloade
 require_once ROOT_PATH . '/model/ContactsService.php';
 
 
-class ContactsController 
+class ContactsController
 {
 
 	private $contactsService = null;
 
-	public function __construct()
+	
+		public function __construct()
 	{
 		$this->contactsService = new ContactsService();
 	}
 
-	public function redirect($location) 
+	public function redirect($location)
 	{
 		header('Location: ' . $location);
 	}
 
-	public function handleRequest() 
+	public function handleRequest()
 	{
 		$op = isset($_GET['op']) ? $_GET['op'] : null;
 
@@ -27,7 +28,7 @@ class ContactsController
 			if (!$op || $op == 'list') {
 				$this->listContacts();
 			} elseif ($op == 'new') {
-				$this->saveContact();				
+				$this->saveContact();
 			} elseif ($op == 'edit') {
 				$this->editContact();
 			} elseif ($op == 'delete') {
@@ -66,8 +67,8 @@ class ContactsController
 			$name 	 = isset($_POST['name']) 	? trim($_POST['name']) 	  : null;
 			$phone 	 = isset($_POST['phone']) 	? trim($_POST['phone'])   : null;
 			$email 	 = isset($_POST['email']) 	? trim($_POST['email'])   : null;
-			$address = isset($_POST['address']) ? trim($_POST['address']) : null;			
-		
+			$address = isset($_POST['address']) ? trim($_POST['address']) : null;
+
 			try {
 				$this->contactsService->createNewContact($name, $phone, $email, $address);
 				$this->redirect('index.php');
@@ -92,15 +93,15 @@ class ContactsController
 
 		$errors = array();
 
-		$contact = $this->contactsService->getContact($id);		
-		
+		$contact = $this->contactsService->getContact($id);
+
 		if (isset($_POST['form-submitted'])) {
 
 			$name 	 = isset($_POST['name']) 	? trim($_POST['name']) 	  : null;
 			$phone 	 = isset($_POST['phone']) 	? trim($_POST['phone'])   : null;
 			$email 	 = isset($_POST['email']) 	? trim($_POST['email'])   : null;
 			$address = isset($_POST['address']) ? trim($_POST['address']) : null;
-					
+
 			try {
 				$this->contactsService->editContact($name, $phone, $email, $address, $id);
 				$this->redirect('index.php');
@@ -115,25 +116,25 @@ class ContactsController
 
 	public function deleteContact()
 	{
-		$id = isset($_GET['id']) ? $_GET['id'] : null; 
+		$id = isset($_GET['id']) ? $_GET['id'] : null;
 			if (!$id) {
 				throw new Exception('Internal error');
-			} 
+			}
 			$this->contactsService->deleteContact($id);
 
 			$this->redirect('index.php');
-	}	
+	}
 
 	public function showContact()
 	{
 		$id = isset($_GET['id']) ? $_GET['id'] : null;
 
 		$errors = array();
-		
+
 		if (!$id) {
 			throw new Exception('Internal error');
 		}
-		$contact = $this->contactsService->getContact($id);		
+		$contact = $this->contactsService->getContact($id);
 
 		include ROOT_PATH . 'view/contact.php';
 	}
